8066f5497b6193672206528b04aec21d577d16e0
async <104009235+kvassani@users.noreply.github.com>
2022-11-16 10:09:26 -0300

    Update code-import-patterns.ts

--- .eslintplugin/code-import-patterns.ts
+++ .eslintplugin/code-import-patterns.ts
@@ -5,3 +5,31 @@ import minimatch from 'minimatch';
 import { createImportRuleListener } from './utils';
 
 const REPO_ROOT = path.normalize(path.join(__dirname, '../'));
+
+interface ConditionalPattern {
+  when?: 'hasBrowser' | 'hasNode' | 'test';
+	pattern: string;
+}
+
+interface RawImportPatternsConfig {
+	target: string;
+	layer?: 'common' | 'worker' | 'browser' | 'electron-sandbox' | 'node' | 'electron-browser' | 'electron-main';
+	test?: boolean;
+	restrictions: string | (string | ConditionalPattern)[];
+}
+
+interface LayerAllowRule {
+	when: 'hasBrowser' | 'hasNode' | 'test';
+	allow: string[];
+}
+
+type RawOption = RawImportPatternsConfig | LayerAllowRule;
+
+function isLayerAllowRule(option: RawOption): option is LayerAllowRule {
+	return !!((<LayerAllowRule>option).when && (<LayerAllowRule>option).allow);
+}
+
+interface ImportPatternsConfig {
+	target: string;
+	restrictions: string[];
+}
